fc54bfd8783892e68506769481fb63647e5d2d65
Paulo Grabin <plgrabin@gmail.com>
2016-06-22 13:22:41 -0300

    Reading the unique title validator.

--- src/main/java/br/com/casadocodigo/loja/controller/ProductsController.java
+++ src/main/java/br/com/casadocodigo/loja/controller/ProductsController.java
@@ -6,6 +6,8 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -14,6 +16,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import br.com.casadocodigo.loja.daos.ProductDAO;
 import br.com.casadocodigo.loja.models.BookType;
 import br.com.casadocodigo.loja.models.Product;
+import br.com.casadocodigo.loja.validations.ProductValidator;
 import br.com.casadocodigo.loja.validations.TitulosIguaisValidator;
 
 @Controller
@@ -52,5 +55,11 @@ public class ProductsController {
 		mv.addObject("products", productDAO.list());
 		
 		return mv;
-	}	
+	}
+	
+	@InitBinder
+	public void initBinder(WebDataBinder binder) {
+		binder.addValidators(new ProductValidator());
+		binder.addValidators(validador);
+	}
 }
