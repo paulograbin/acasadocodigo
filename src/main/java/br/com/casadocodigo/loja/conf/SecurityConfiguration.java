eb35dc8391aa59bbc974073c9dca7a94e22b47bd
Paulo Grabin <plgrabin@gmail.com>
2016-06-24 09:40:34 -0300

    Ex: 11.6 - Maintaining security with the database

--- src/main/java/br/com/casadocodigo/loja/conf/SecurityConfiguration.java
+++ src/main/java/br/com/casadocodigo/loja/conf/SecurityConfiguration.java
@@ -1,13 +1,21 @@
 package br.com.casadocodigo.loja.conf;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import br.com.casadocodigo.loja.daos.UserDAO;
 
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
+	@Autowired
+	private UserDAO users;
+	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
@@ -17,5 +25,12 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 			.antMatchers("/products/**").permitAll()
 			.anyRequest().authenticated()
 			.and().formLogin();
+		
+	}
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(users)
+			.passwordEncoder(new BCryptPasswordEncoder());
 	}
 }
